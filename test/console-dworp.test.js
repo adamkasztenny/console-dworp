a144314f7ef44748aa50992958b941cd4d1aab67
Adam Kasztenny <adamkasztenny@gmail.com>
2019-01-21 18:57:42 -0500

    Add test for additional arguments

--- test/console-dworp.test.js
+++ test/console-dworp.test.js
@@ -1,13 +1,23 @@
 describe("console-dworp", function () {
 
-    it('should dworp', function () {
+    it('should dworp, with no additional arguments', function () {
         spyOn(console, 'debug');
 
         expect(console.dworp).toBeDefined();
         console.dworp();
 
         expect(console.debug).toHaveBeenCalledTimes(1);
-        expect(console.debug).toHaveBeenCalledWith('dworp');
+        expect(console.debug).toHaveBeenCalledWith('dworp', '');
+    });
+    
+    it('should dworp, with additional arguments', function () {
+        spyOn(console, 'debug');
+
+        expect(console.dworp).toBeDefined();
+        console.dworp('some message');
+
+        expect(console.debug).toHaveBeenCalledTimes(1);
+        expect(console.debug).toHaveBeenCalledWith('dworp', 'some message');
     });
 
     it('should export the dworp' , function () {
