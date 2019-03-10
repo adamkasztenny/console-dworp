a2f0c1e0f384888563733deaefd42444f6c7c790
Adam Kasztenny <adamkasztenny@gmail.com>
2019-03-09 19:45:01 -0500

    Fix bug with no variadic args and falsy values not showing up

--- src/console-dworp.js
+++ src/console-dworp.js
@@ -1,26 +1,21 @@
-console.dworp = function (additionalOutput) {
+console.dworp = function (...additionalOutput) {
     debug("dworp", additionalOutput)
 }
 
-console.blorp = function (additionalOutput) {
+console.blorp = function (...additionalOutput) {
     debug("blorp", additionalOutput)
 }
 
-console.florp = function (additionalOutput) {
+console.florp = function (...additionalOutput) {
     debug("florp", additionalOutput)
 }
 
-console.derp = function (additionalOutput) {
+console.derp = function (...additionalOutput) {
     debug("derp", additionalOutput)
 }
 
 function debug(type, additionalOutput) {
-    if (additionalOutput) {
-        console.debug(type, additionalOutput);
-    }
-    else {
-        console.debug(type);
-    }
+    console.debug(type, ...additionalOutput);
 }
 
 module.exports = function () {
